5003ba0394d91a4c5ec974c620bc7005c2459852
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-06-11 03:43:15 +0200

    all projecte

---
+++ src/ex1/LinkedListTest.java
@@ -0,0 +1,142 @@
+package ex1;
+
+import org.junit.jupiter.api.Assertions;
+
+class LinkedListTest {
+    @org.junit.jupiter.api.Test
+    void instantiate() {
+        LinkedList l = new LinkedList("Node 1");
+        Assertions.assertEquals(1, l.getSize());
+        Assertions.assertEquals("Node 1", l.find(0).getData());
+    }
+
+    @org.junit.jupiter.api.Test
+    void addAtHead() {
+        //First insert (creation)
+        LinkedList l = new LinkedList("Node 1");
+
+        l.addAtHead("Node 0");
+        Assertions.assertEquals(2, l.getSize());
+        Assertions.assertEquals("Node 0", l.find(0).getData());
+        Assertions.assertEquals("Node 1", l.find(1).getData());
+    }
+
+    @org.junit.jupiter.api.Test
+    void addAtTail() {
+        //First insert (creation)
+        LinkedList l = new LinkedList("Node 0");
+
+        l.addAtTail("Node 1");
+        Assertions.assertEquals(2, l.getSize());
+        Assertions.assertEquals("Node 0", l.find(0).getData());
+        Assertions.assertEquals("Node 1", l.find(1).getData());
+    }
+
+    @org.junit.jupiter.api.Test
+    void addAtIndex() {
+        //First insert (creation)
+        LinkedList l = new LinkedList("Node 1");
+
+        //Insert at first
+        l.addAtIndex(0, "Node 0");
+        Assertions.assertEquals(2, l.getSize());
+        Assertions.assertEquals("Node 0", l.find(0).getData());
+        Assertions.assertEquals("Node 1", l.find(1).getData());
+
+        //Insert at last
+        l.addAtIndex(l.getSize(), "Node 3");
+        Assertions.assertEquals(3, l.getSize());
+        Assertions.assertEquals("Node 0", l.find(0).getData());
+        Assertions.assertEquals("Node 1", l.find(1).getData());
+        Assertions.assertEquals("Node 3", l.find(2).getData());
+
+        //Insert in the middle
+        l.addAtIndex(2, "Node 2");
+        Assertions.assertEquals(4, l.getSize());
+        Assertions.assertEquals("Node 0", l.find(0).getData());
+        Assertions.assertEquals("Node 1", l.find(1).getData());
+        Assertions.assertEquals("Node 2", l.find(2).getData());
+        Assertions.assertEquals("Node 3", l.find(3).getData());
+
+        //Insert at < first
+        l.addAtIndex(-99, "Node -99");
+        Assertions.assertEquals(5, l.getSize());
+        Assertions.assertEquals("Node -99", l.find(0).getData());
+        Assertions.assertEquals("Node 0", l.find(1).getData());
+        Assertions.assertEquals("Node 1", l.find(2).getData());
+        Assertions.assertEquals("Node 2", l.find(3).getData());
+        Assertions.assertEquals("Node 3", l.find(4).getData());
+
+        //Insert at > last
+        l.addAtIndex(99, "Node 99");
+        Assertions.assertEquals(6, l.getSize());
+        Assertions.assertEquals("Node -99", l.find(0).getData());
+        Assertions.assertEquals("Node 0", l.find(1).getData());
+        Assertions.assertEquals("Node 1", l.find(2).getData());
+        Assertions.assertEquals("Node 2", l.find(3).getData());
+        Assertions.assertEquals("Node 3", l.find(4).getData());
+        Assertions.assertEquals("Node 99", l.find(5).getData());
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteAtIndex() {
+        //Creating initial list
+        LinkedList l = new LinkedList("Node 0");
+        l.addAtTail("Node 1");
+        l.addAtTail("Node 2");
+        l.addAtTail("Node 3");
+        l.addAtTail("Node 4");
+        l.addAtTail("Node 5");
+        Assertions.assertEquals(6, l.getSize());
+
+        //Delete the last
+        l.deleteAtIndex(l.getSize() - 1);
+        Assertions.assertEquals(5, l.getSize());
+        Assertions.assertEquals("Node 0", l.find(0).getData());
+        Assertions.assertEquals("Node 1", l.find(1).getData());
+        Assertions.assertEquals("Node 2", l.find(2).getData());
+        Assertions.assertEquals("Node 3", l.find(3).getData());
+        Assertions.assertEquals("Node 4", l.find(4).getData());
+
+        //Delete the first
+        l.deleteAtIndex(0);
+        Assertions.assertEquals(4, l.getSize());
+        Assertions.assertEquals("Node 1", l.find(0).getData());
+        Assertions.assertEquals("Node 2", l.find(1).getData());
+        Assertions.assertEquals("Node 3", l.find(2).getData());
+        Assertions.assertEquals("Node 4", l.find(3).getData());
+
+        //Delete from the middle
+        l.deleteAtIndex(2);
+        Assertions.assertEquals(3, l.getSize());
+        Assertions.assertEquals("Node 1", l.find(0).getData());
+        Assertions.assertEquals("Node 2", l.find(1).getData());
+        Assertions.assertEquals("Node 4", l.find(2).getData());
+
+        //Delete > last
+        l.deleteAtIndex(99);
+        Assertions.assertEquals(2, l.getSize());
+        Assertions.assertEquals("Node 1", l.find(0).getData());
+        Assertions.assertEquals("Node 2", l.find(1).getData());
+
+        //Delete < first
+        l.deleteAtIndex(-99);
+        Assertions.assertEquals(1, l.getSize());
+        Assertions.assertEquals("Node 2", l.find(0).getData());
+    }
+
+    @org.junit.jupiter.api.Test
+    void _toString() {
+        LinkedList l = new LinkedList("Node 1");
+        Assertions.assertEquals("Node 1", l.toString());
+
+        l.addAtTail("Node 2");
+        Assertions.assertEquals("Node 1 => Node 2", l.toString());
+
+        l.addAtTail("Node 3");
+        Assertions.assertEquals("Node 1 => Node 2 => Node 3", l.toString());
+
+        l.addAtTail("Node 4");
+        Assertions.assertEquals("Node 1 => Node 2 => Node 3 => Node 4", l.toString());
+    }
+}
---
+++ src/ex2/LinkedList.java
@@ -0,0 +1,136 @@
+package ex2;
+
+/*============================================================================
+Name        : LinkedList.java
+Author      : Eric Swanson
+Date	    : Sep 7, 2011
+Version     :
+Description :
+Copyright (C) 2011 netherwolfe
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+============================================================================*/
+//Source: https://gist.github.com/es20641/1208340/06d598126d53b048058bc243cbc4e4dd7db9a23a
+
+class LinkedList
+{
+    //Class variables for the Linked List
+    private Node head;
+    private int numNodes;
+
+    public LinkedList(Object dat)
+    {
+        head = new Node(dat);
+        numNodes++;
+    }
+
+    public void addAtHead(Object dat)
+    {
+        Node temp = head;
+        head = new Node(dat);
+        head.next = temp;
+        numNodes++;
+    }
+
+    public void addAtTail(Object dat)
+    {
+        Node temp = head;
+        while(temp.next != null)
+        {
+            temp = temp.next;
+        }
+
+        temp.next = new Node(dat);
+        numNodes++;
+    }
+
+    public void addAtIndex(int index, Object dat)
+    {
+        Node temp = null;
+        if(index <= 0){
+            temp = head;
+            head = new Node(dat);
+            head.next = temp;
+        }
+        else {
+            temp = getPrevious(index);
+            Node holder = temp.next;
+            temp.next = new Node(dat);
+            temp.next.next = holder;
+        }
+        numNodes++;
+    }
+
+    public void deleteAtIndex(int index)
+    {
+        if(index <= 0) head = head.next;
+        else {
+            index = Math.min(index, getSize()-1);
+            Node temp = getPrevious(index);
+            temp.next = temp.next.next;
+        }
+        numNodes--;
+    }
+
+    private Node getPrevious(int index) {
+        Node temp = head;
+        for(int i=0; i< index - 1 && temp.next != null; i++)
+        {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public Node find(int index)
+    {
+        Node temp=head;
+        for(int i=0; i<index; i++)
+        {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public String toString()
+    {
+        Node n = head;
+        String s = n.data.toString();
+
+        while(n.next != null){
+            n = n.next;
+            s += " => " + n.data.toString();
+        }
+
+        return  s;
+    }
+
+    public int getSize()
+    {
+        return numNodes;
+    }
+
+    class Node
+    {
+        //Declare class variables
+        private Node next;
+        private Object data;
+
+        public Node(Object dat)
+        {
+            data = dat;
+        }
+
+        public Object getData()
+        {
+            return data;
+        }
+    }
+}
